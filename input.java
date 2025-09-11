de6299ab6c23b26d4b69776a29e3d8b71975fd45
mohit000467 <mohitsingh97941111@gmail.com>
2025-09-12 00:14:18 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[]arg){
+        Scanner s = new Scanner(System.in);
+        System.out.print("enter your number : ");
+        int number  = s.nextInt();
+        System.out.println("number is " + number);
+    }
+}
