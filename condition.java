98b6a95702fb7ceb214bc8f74780f1977c34e94d
mohit000467 <mohitsingh97941111@gmail.com>
2025-09-12 16:32:03 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,9 @@
+public class condition {
+    public static void main(String[] args){
+        boolean day = true;
+        if(day == true)
+            System.out.print("yes");
+        else
+            System.out.print("no");
+    }
+}
