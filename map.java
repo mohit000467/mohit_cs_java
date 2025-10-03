f04261f7f7f700d99335dccebc208383876eff9f
mohit000467 <mohitsingh97941111@gmail.com>
2025-10-03 09:44:20 +0530

    Add files via upload

---
+++ map.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class map {
+public static void main(String[]arg){
+    Map<String, Integer> map = new HashMap<>();
+    map.put("mohit" , 155);
+    map.put("sharma" , 156);
+    System.out.print(map);
+}
+}
