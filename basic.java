22c67b2a0d172844b2e7b0e84f70df73a51be5af
mohit000467 <mohitsingh97941111@gmail.com>
2025-10-15 09:20:56 +0530

    Add files via upload

--- basic.java
+++ basic.java
@@ -1,5 +1,25 @@
+class A extends Thread{
+    public void run(){
+        System.out.println("Running");
+        try{
+            Thread.sleep(2000);
+        }catch(InterruptedException e){
+            System.out.println(e);
+        }
+    }
+}
+
+
 public class basic {
-    public static void main(String[] args) {
-        System.out.println("helllo world");
+    public static void main(String[]arg)throws InterruptedException{
+        A t1 = new A();
+        System.out.println(t1.getState());
+        t1.start();
+         System.out.println(t1.getState());
+         Thread.sleep(1000);
+         System.out.println(t1.getState());
+         t1.join();;
+         System.out.println(t1.getState());
+
     }
 }
